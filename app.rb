697e2e8149fdbea2180fcc6798359e7204367c44
Vaclav Hodina <vhodina@redhat.com>
2017-03-04 23:58:19 +0100

    Added documentation to app.rb file

--- app.rb
+++ app.rb
@@ -6,46 +6,67 @@ require 'slim'
 
 include FioHelper
 
+##
+# Sinatra application configuration
+# This is the main file of the invoice reminder application
+
 database = DatabaseHelper.new
 user = ''
 
+##
+# Using Rack functionality for basic authentication
 use Rack::Auth::Basic, 'Restricted Area' do |username, password|
   database.check_user(username, password)
   user = database.get_user(username)
 end
 
+##
+# Show dashboard page
 get '/' do
   @logged_user = user
   @account = database.get_account_info()
   slim :app
 end
 
+##
+# Show administration page, where the admin sees the list of admins and users
 get '/admin' do
   @logged_user = user
   @users = database.get_users
   slim :admin
 end
 
+##
+# Route called to create new users and administrators, ie. new user accounts
 post '/admin/create' do
   database.create_user(params['username'], params['password'], params['admin'])
   redirect '/admin'
 end
 
+##
+# Post method called to change user account into admin account
 post '/admin/make_admin/:username' do
   database.update_user(params['username'], true)
   redirect '/admin'
 end
 
+##
+# Past method called to change admin account to user account, ie. lose admin access
 post '/admin/make_user/:username' do
   database.update_user(params['username'], false)
   redirect '/admin'
 end
 
+##
+# Method called to delete user/admin account
 post '/admin/delete/:username' do
   database.delete_user(params['username'])
   redirect '/admin'
 end
 
+##
+# Route to page with transactions depending on the variable, it can show all, negative
+# or positive transactions
 get '/:filter' do
   @logged_user = user
   if params['filter'] == 'all'
@@ -60,6 +81,8 @@ get '/:filter' do
   slim :transactions
 end
 
+##
+# Post route for uploading a file with invoice, or any other information to transaction
 post '/upload/:id' do
   if params[:file]
     @filename = params[:file][:filename]
@@ -75,11 +98,15 @@ post '/upload/:id' do
   slim :confirmation
 end
 
+##
+# Get method for downloading previously uploaded files
 get '/download/:id' do
   file = database.get_filepath(params['id'])
   send_file(file, :disposition => 'attachment')
 end
 
+##
+# Method for deleting previously uploaded file
 post '/delete/:id' do
   file = database.get_filepath(params['id'])
   File.delete(file)
@@ -87,6 +114,8 @@ post '/delete/:id' do
   slim :confirmation
 end
 
+##
+# Route showing a confirmation page after file has been uploaded
 get '/confirmation' do
   @logged_user = user
   slim :confirmation
