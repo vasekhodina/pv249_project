22405e583452790d552da29e6501c8803d1d9444
Vaclav Hodina <vhodina@redhat.com>
2017-03-05 11:46:12 +0100

    Clean the code of fio_helper.rb according to rubocop, only remaining offence: line too long.

--- fio_helper.rb
+++ fio_helper.rb
@@ -7,30 +7,29 @@ TOKEN = ENV['TOKEN']
 ##
 # Module for getting information from fio bank account
 module FioHelper
-  URL_GET_TRN='https://www.fio.cz/ib_api/rest/last/#token#/transactions.json'
-  URL_SET_DAY='https://www.fio.cz/ib_api/rest/set-last-date/#token#/#rrrr-mm-dd#/'
-
   class << self
     ##
-    # Downloads a list of transactions in form of a JSON file, parses it and returns a hash.
-    # If since is not nil, sets the date from which the transactions should be downloaded.
+    # Downloads a list of transactions in form of a JSON file, parses it and
+    # returns a hash.
+    # If since is not nil, sets the date from which the transactions should be
+    # downloaded.
     # If since arg. is nil, then downloads transactions since the last download
     def get_transactions(since)
-      if since
-        set_day(since)
-      end
-      response = Typhoeus::Request.new(URL_GET_TRN.sub!('#token#',TOKEN)).run.response_body
+      get_trn_url = 'https://www.fio.cz/ib_api/rest/last/#token#/transactions.json'
+      fio_set_day(since) if since
+      response = Typhoeus::Request.new(get_trn_url.sub!('#token#', TOKEN)).run.response_body
       JSON.parse(response)
     end
 
     ##
     # Sets a day from which the next update should be done.
-    # This is a functionality provided by Fio API. More info at https://www.fio.cz/bank-services/internetbanking-api
-    def set_day(from)
-      URL_SET_DAY.sub!('#rrrr-mm-dd#', from)
-      URL_SET_DAY.sub!('#token#',TOKEN)
-      response = Typhoeus::Request.new(URL_SET_DAY).run.response_body
-      return response
+    # This is a functionality provided by Fio API.
+    # More info at https://www.fio.cz/bank-services/internetbanking-api
+    def fio_set_day(from)
+      set_day_url = 'https://www.fio.cz/ib_api/rest/set-last-date/#token#/#rrrr-mm-dd#/'
+      set_day_url.sub!('#rrrr-mm-dd#', from)
+      set_day_url.sub!('#token#', TOKEN)
+      Typhoeus::Request.new(set_day_url).run.response_body
     end
   end
 end
